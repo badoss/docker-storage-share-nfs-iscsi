6874407e913ae2d07044b2b63d68d202d6df77e3
boo boo <thongrit.th@inet.co.th>
2022-01-14 03:23:37 +0700

    add webapp and blackend

---
+++ blackend/main.py
@@ -0,0 +1,12 @@
+import os
+import sys
+
+def ConnectDB():
+    print('stert connect DB ')
+
+
+if __name__ == "__main__":
+    ConnectDB()
+    print("Executed when invoked directly")
+else:
+    print("Executed when imported")
